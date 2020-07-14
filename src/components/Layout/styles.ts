8d57a718eeb3da49dfece61bf91f64d21e64fd21
Lucas Bicalho <lucassbcosta@hotmail.com>
2020-07-14 18:25:00 -0300

    layout created

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background: var(--primary);
+`
+
+export const Wrapper = styled.div`
+  height: 100%;
+  width: 1280px;
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: center;
+`
+
